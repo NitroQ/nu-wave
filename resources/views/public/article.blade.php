c764450af30911822e061d7dde5fdbc1df1c2349
NitroQ <frankgesmundo10@gmail.com>
2022-05-27 15:14:50 +0800

    fixed article

--- resources/views/public/article.blade.php
+++ resources/views/public/article.blade.php
@@ -35,28 +35,27 @@
        </div>
     </div>
 </div>
-    <div class="container-fluid p-4">
-        <div class="row justify-content-center">
-              @foreach ($articles as $a)
-            <div class="col-lg-6">
-              <div class="row border border-dark rounded py-3 m-lg-0 mt-lg-3 m-3">
-                  <div class="col-lg-4 d-flex justify-content-center" >
-                      <img src="/uploads/articles/{{ $a->image or 'placeholder.png' }}" alt="" id="article-img" class="img-fluid">
-                  </div>
-                  <div class="col-lg-8" >
-                      <div class="container pt-5 pt-lg-0">
-                          <h5 class="fw-bold">{{ $a->title }}</h5>
-                          <small class="date-sm">{{ $a->created_at->format('F j, Y') }} &nbsp; {{ $a->category }} </small> <br>
-                          <small class="date-sm">{{ $a->tag }}</small>
-                          <p class="">{!! mb_strimwidth(strip_tags($a->description), 0, 220, '...') !!}</p>
-                          <a href="{{ route('article-view' , [$a->id]) }}" class="text-decoration-none text-muted">Read Details ></a>
-                      </div>
-                      
-                  </div>
-              </div>
+    <div class="container pb-5 pt-3">
+      <div class="row justify-content-center ">
+          <div class="col-lg-8">
+            @foreach ($articles as $a)
+            <div class="row border border-dark rounded py-3 m-lg-0 mt-lg-3 m-3">
+                <div class="col-lg-4 d-flex justify-content-center" >
+                    <img src="/uploads/articles/{{ $a->image or 'placeholder.png' }}" alt="" id="article-img" class="img-fluid">
+                </div>
+                <div class="col-lg-8" >
+                    <div class="container pt-5 pt-lg-0">
+                        <h5 class="fw-bold">{{ $a->title }}</h5>
+                        <small class="date-sm">{{ $a->created_at->format('F j, Y') }} &nbsp; {{ $a->category }} </small> <br>
+                        <small class="date-sm">{{ $a->tag }}</small>
+                        <p class="">{!! mb_strimwidth(strip_tags($a->description), 0, 220, '...') !!}</p>
+                        <a href="{{ route('article-view' , [$a->id]) }}" class="text-decoration-none text-muted">Read Details ></a>
+                    </div>
+                    
+                </div>
             </div>
-              @endforeach
-            
-        </div>
+            @endforeach
+          </div>
+      </div>
     </div>
 @endsection
